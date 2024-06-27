8a2c341576e52eccf81d4c85618748daf899ee6a
Amritansh Verma <amritansh.cms28@gmail.com>
2024-06-27 16:01:49 +0530

    Create funcs.py

---
+++ funcs.py
@@ -0,0 +1,23 @@
+import pickle
+
+def save(obj, file_path):
+    """
+    Save a Python object to a pickle file.
+
+    :param obj: Python object to be saved
+    :param file_path: Path where the pickle file will be saved
+    """
+    with open(file_path, 'wb') as file:
+        pickle.dump(obj, file)
+
+def load(file_path):
+    """
+    Read a Python object from a pickle file and print the type of the data.
+
+    :param file_path: Path to the pickle file
+    :return: The Python object read from the pickle file
+    """
+    with open(file_path, 'rb') as file:
+        data = pickle.load(file)
+        print(f'The type of the data is: {type(data)}')
+        return data
